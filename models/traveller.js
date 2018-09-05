6e60f38c8f7fdb8b3174e37b776a07490b50d3cb
Pat <graczyk.pat@gmail.com>
2018-09-05 17:26:23 +0100

    enumartion hmwrk done

---
+++ models/traveller.js
@@ -0,0 +1,50 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  // return this.journeys.map((journey)  => {return journey});
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getModesOfTransport = function () {
+  return this.journeys.map((journey) => {
+    return journey.transport;
+  })
+};
+
+Traveller.prototype.getJourneysByTransport = function(transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport == transport;
+  })
+};
+
+Traveller.prototype.getJourneysByMinDistance = function(minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  })
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total,journey) => {
+    return total += journey.distance }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+    return this.journeys.some(((journey) => {
+      return journey.transport;
+    })
+};
+
+
+
+
+module.exports = Traveller;
